dc15b040772a43f3c76aca037c92eba00a55a55d
Mario <m.amadeo54@gmail.com>
2025-03-21 13:32:59 +0100

    Segundo commit

--- prueba.js
+++ prueba.js
@@ -1,6 +1,8 @@
 console.log("Hola mundo");
 
 let texto="HOLA";
-console.log(texto);
+console.log(texto2);
 alert(texto);
 
+let texto2="Primera prueba github, asociando github,carpetas y codigo";
+alert(texto2);
